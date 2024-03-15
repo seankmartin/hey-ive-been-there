1ba2961e2ff9bf10ff7140f68e1d1a2fd82650b1
Sean Martin <martins7@tcd.ie>
2024-03-15 11:04:18 +0100

    refactor: start structure

---
+++ src/Experience/Experience.ts
@@ -0,0 +1,9 @@
+import * as THREE from 'three'
+
+export default class Experience {
+    canvas: HTMLCanvasElement
+    constructor(canvas: HTMLCanvasElement) {
+        this.canvas = canvas
+        console.log('Experience')
+    }
+    }
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,2 @@
-import * as THREE from 'three'
-import GUI from 'lil-gui'
-
-/**
- * Base
- */
-// Debug
-const gui = new GUI()
-
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
-
-// Scene
-const scene = new THREE.Scene()
+import Experience from './Experience/Experience'
+const experience = new Experience(document.querySelector('canvas.webgl')!)
