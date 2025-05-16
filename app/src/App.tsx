9f238e10a164e6fbc24a7cfa59d6140061ecd386
Sean Martin <martins7@tcd.ie>
2025-05-16 12:08:10 +0200

    feat: visual updates

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,20 +1,29 @@
 import { Canvas } from "@react-three/fiber";
-import { Stage, OrbitControls } from "@react-three/drei";
+import { Stage, OrbitControls, Stars } from "@react-three/drei";
 import Earth from "./Earth";
 import { Perf } from "r3f-perf";
 
 function App() {
   return (
     <Canvas>
+      <Stars
+        radius={70}
+        depth={10}
+        count={10000}
+        factor={4}
+        saturation={1}
+        fade
+        speed={1}
+      />
       <Stage
-        shadows={{ type: "contact", opacity: 0.8, blur: 3 }}
+        preset={"rembrandt"}
+        adjustCamera={1}
         environment="night"
+        intensity={0.1}
       >
-        <ambientLight intensity={Math.PI} />
-        <directionalLight position={[10, 10, 10]} intensity={1.5} />
         <Earth />
       </Stage>
-      <OrbitControls />
+      <OrbitControls makeDefault />
       <Perf position="top-left" />
     </Canvas>
   );
